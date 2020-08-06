78d32c543af630f1921e0bf91f442ee24311e4b6
Dr. Vural Aksakalli <vaksakalli@gmail.com>
2020-08-06 19:05:08 +1000

    Add files via upload

--- spFtSel_example.py
+++ spFtSel_example.py
@@ -1,8 +1,8 @@
 
-# SpFtSel: Feature Selection and Ranking via SPSA
+# SpFtSel: Feature Selection and Ranking via SPSA-BB
 # Required Python version >= 3.6
 # V. Aksakalli & Z. D. Yenice
-# GPL-3.0, 2019
+# GPL-3.0, 2020
 # Please refer to below for more information:
 # https://arxiv.org/abs/1804.05589
 
@@ -24,15 +24,15 @@ np.random.seed(8)
 df = load_breast_cancer()
 x, y = df.data, df.target
 
-# specify a wrapper to use
+# specify a _wrapper to use
 wrapper = DecisionTreeClassifier()
 
 # specify a metric to maximize
 # (by default, sklearn metrics are defined as "higher is better")
-# you need to make sure your scoring metric is consistent with your problem type,
+# you need to make sure your _scoring metric is consistent with your problem type,
 # based on whether it is a binary or multi-class classification problem
 # example: accuracy, f1, roc_auc, etc.
-# more info on the scoring metrics can be found here:
+# more info on the _scoring metrics can be found here:
 # https://scikit-learn.org/stable/modules/model_evaluation.html
 scoring = 'accuracy'
 
@@ -44,13 +44,13 @@ sp_engine = SpFtSel(x, y, wrapper, scoring)
 # 1. num_features: how many features to select
 #    (in addition to features to keep, if any)
 #    default value is 0 and it results in automatic feature selection
-# 2. run_mode: 'regular' (default) or 'extended'
+# 2. run_mode: 'regular' (default) or 'short'
 # 3. stratified_cv: whether CV should be stratified or not (default is True)
-#    stratified_cv must be set to False for regression problems
+#    stratified_cv MUST be set to False for regression problems
 # 4. n_jobs: number of cores to be used in cross-validation (default is 1)
 # 5. print_freq: print frequency for the output (default is 5)
 # 6. features_to_keep_indices: indices of features to keep: default is None
-sp_run = sp_engine.run(num_features=5)
+sp_run = sp_engine.run(num_features=5, run_mode='short')
 
 # get the results of the run
 sp_results = sp_run.results
@@ -84,13 +84,13 @@ x, y = df.data, df.target
 
 wrapper = DecisionTreeRegressor()
 
-scoring = 'neg_mean_squared_error'
+scoring = 'r2'
 
 sp_engine = SpFtSel(x, y, wrapper, scoring)
 
 # for regression problems, you must set stratified_cv to False
 # because the default value of True will not work
-sp_run = sp_engine.run(num_features=5, stratified_cv=False)
+sp_run = sp_engine.run(num_features=5, run_mode='short', stratified_cv=False)
 
 sp_results = sp_run.results
 
