c774f7de632f472ce48d2206359177fc123605ce
Dr. Vural Aksakalli <vaksakalli@gmail.com>
2020-08-19 02:12:06 +1000

    Update SpFtWgt_example_github.py

--- SpFtWgt_example_github.py
+++ SpFtWgt_example_github.py
@@ -44,12 +44,16 @@ sp_engine = SpFtSel(x, y, wrapper, scoring)
 # 1. num_features: how many features to select
 #    (in addition to features to keep, if any)
 #    default value is 0 and it results in automatic feature selection
+# 2. iter_max: max number of iterations
+#    for small datasets, iter_max = 150 works well (default)
+#    for large datasets, iter_max = 300 works well
+#    iteration stall limit will be iter_max/3
 # 3. stratified_cv: whether CV should be stratified or not (default is True)
 #    stratified_cv MUST be set to False for regression problems
 # 4. n_jobs: number of cores to be used in cross-validation (default is 1)
 # 5. print_freq: print frequency for the output (default is 5)
 # 6. features_to_keep_indices: indices of features to keep: default is None
-sp_run = sp_engine.run(num_features=5)
+sp_run = sp_engine.run(num_features=5, iter_max=150)
 
 # get the results of the run
 sp_results = sp_run.results
@@ -87,8 +91,10 @@ scoring = 'r2'
 
 sp_engine = SpFtSel(x, y, wrapper, scoring)
 
-# for regression problems, you must set stratified_cv to False
-# because the default value of True will not work
+# for regression problems:
+# you MUST set stratified_cv to False
+# as the default value of True will not work
+# you should also scale y to be between 0 and 1 for the algorithm to work properly!
 sp_run = sp_engine.run(num_features=5, stratified_cv=False)
 
 sp_results = sp_run.results
