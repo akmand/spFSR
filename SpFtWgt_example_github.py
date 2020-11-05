e5978b81e059b616a4777a9e285fae01a53e1a3c
Dr. Vural Aksakalli <vaksakalli@gmail.com>
2020-11-06 00:19:27 +1100

    Update SpFtWgt_example_github.py

--- SpFtWgt_example_github.py
+++ SpFtWgt_example_github.py
@@ -1,112 +1,173 @@
 
-# SpFSR: Feature Selection and Ranking via SPSA-BB
+# SpFtWgt: Feature Weighting and Feature Selection via SPSA
 # Required Python version >= 3.6
-# V. Aksakalli & Z. D. Yenice
+# G. Yeo & V. Aksakalli
 # GPL-3.0, 2020
-# Please refer to below for more information:
-# https://arxiv.org/abs/1804.05589
-
 
 import numpy as np
-from sklearn.datasets import load_breast_cancer, load_boston
-from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
+from sklearn.model_selection import RepeatedKFold, RepeatedStratifiedKFold, cross_val_score
 
-from spFSR import SpFSR
+from SpFtWgt import SpFtWgt
+
+# Source for prepare_dataset_for_modeling.py:
+# https://github.com/vaksakalli/datasets/blob/master/prepare_dataset_for_modeling_github.py
+from prepare_dataset_for_modeling_github import prepare_dataset_for_modeling
 
 
 #################################################
 # ##### CLASSIFICATION EXAMPLE  #################
 #################################################
 
-df = load_breast_cancer()
-x, y = df.data, df.target
+dataset = 'wine.csv'
+n_neighbors = 1
+n_jobs = 1
+####
+x, y = prepare_dataset_for_modeling(dataset, pred_type='c')
+print("dataset = " + dataset + ", shape of x = ", x.shape)
 
-# specify prediction type
-# pred_type needs to be 'c' for classification and 'r' for regression datasets
-pred_type = 'c'
+# specify a wrapper classifier to use
+wrapper = KNeighborsClassifier(n_neighbors=n_neighbors)
 
 # specify a metric to maximize
-# by default, sklearn metrics are defined as "higher is better"
-# a 'None' value for scoring will default to 'accuracy' for classification and 'r2' (r-squared) for regression datasets
+# (by default, sklearn metrics are defined as "higher is better")
 # you need to make sure your _scoring metric is consistent with your problem type
 # example: accuracy, f1, roc_auc, etc.
-# more info on the scoring metrics can be found here:
+# more info on the _scoring metrics can be found here:
 # https://scikit-learn.org/stable/modules/model_evaluation.html
 scoring = 'accuracy'
 
-# specify a wrapper to use
-# a 'None' value for the wrapper will default to a random forest wrapper
-# this way spFSR will behave as a FILTER feature selection method
-wrapper = DecisionTreeClassifier(random_state=1)
-
 # set the engine parameters
-sp_engine = SpFSR(x, y, pred_type=pred_type, scoring=scoring, wrapper=wrapper)
+sp_engine = SpFtWgt(x=x, y=y, wrapper=wrapper, scoring=scoring)
 
-# run the spFSR engine
-# available engine parameters (with default values in parentheses):
-# 1.  num_features (0): number of features to select - a value of 0 results in automatic feature selection
-# 2.  iter_max (100): max number of iterations
-# 3.  stall_limit (30): when to restart the search (up to iter_max) - should be about iter_max/3
-# 4.  n_samples_max (5000): max number of randomly selected observations to be used during search
-#     can be 'None' for using all available observations
-# 5.  ft_weighting (False): if features should be weighted by their importance values - this usually helps with kNN's.
-# 6.  use_hot_start (True): if hot start is to be used where initial feature importance vector is
-#     determined by random forest importance (RFI) - this usually results in faster convergence
-# 7.  hot_start_range (0.2): range for the initial feature importance vector in case of a hot start
-#     for example: for a range of 0.2, most important RFI feature will have an imp. value of 0.1 and
-#     the least important will have an imp. value of -0.1 - a value of 0 is also possible and
-#     it will result in all RFI-selected features to have 0 imp. values
-# 8.  gain_type ('bb'): either 'bb' (Barzilai & Borwein) gains or 'mon' (monotone) gains as the step size during search
-# 9.  cv_folds (5): number of folds to use during (perhaps repeated) CV both for evaluation and gradient evaluation
-# 10.  num_grad_avg (4): number of gradient estimates to be averaged for determining search direction
-#     for better gradient estimation, try increasing this number - though this will slow down the search
-# 11. cv_reps_eval (3): number of CV repetitions for evaluating a candidate feature set
-# 12. cv_reps_grad (1): number of CV repetitions for evaluating y-plus and y-minus solutions during gradient estimation
-# 13. stall_tolerance (1e-8): tolerance in objective function change for stalling
-# 14. display_rounding (3): number of digits to display during algorithm execution
-# 15. is_debug (False): whether detailed search info should be displayed for each iteration
-# 16. random_state(1): seed for controlling randomness in the execution of the algorithm
-# 17. n_jobs (1): number of cores to be used in CV - this will be passed into cross_val_score()
-# 18. print_freq (10): iteration print frequency for the algorithm output
-sp_run = sp_engine.run(num_features=5)
+# make sure the results are repeatable
+np.random.seed(123)
 
-# get the results of the run
-sp_results = sp_run.results
+# run the engine
+# available engine parameters:
+# 1. num_features: how many features to select
+#    (in addition to features to keep, if any)
+#    default value is 0 and it results in automatic feature selection
+# 2. iter_max: max number of iterations
+#    for small datasets, iter_max = 150 works well
+#    for large datasets, iter_max = 300 works well (default)
+#    iteration stall limit will be iter_max/3
+# 3. stratified_cv: whether CV should be stratified or not (default is True)
+#    stratified_cv MUST be set to False for regression problems
+# 4. n_jobs: number of cores to be used in cross-validation (default is 1)
+# 5. print_freq: print frequency for the output (default is 5)
+# 6. features_to_keep_indices: indices of features to keep: default is None
+# 7. fs_threshold: feature selection threshold (default is 0.5)
+SpFtWgt_results = sp_engine.run(n_jobs=n_jobs).results
 
 # list of available keys in the engine output
-print('Available keys:', sp_results.keys())
-
+print('Available keys:\n', SpFtWgt_results.keys())
 # performance value of the best feature set
-print('Best value:', sp_results.get('selected_ft_score_mean'))
-
+print('Best value:', SpFtWgt_results.get('best_value'))
 # indices of selected features
-print('Indices of selected features: ', sp_results.get('selected_features'))
-
+print('Indices of selected features: ', SpFtWgt_results.get('features'))
 # importance of selected features
-print('Importance of selected features: ', sp_results.get('selected_ft_importance').round(3))
+print('Weights of selected features: ', SpFtWgt_results.get('importance').round(3))
+# # gain sequence used during optimization
+# print('BB Gains:', SpFtWgt_results.get('iter_results').get('gains'))
 
-# number of iterations for the optimal set
-print('Total iterations for the optimal feature set:', sp_results.get('total_iter_for_opt'))
+#################################################
+# ######   Comparisons   ########################
+#################################################
 
-print('\n')
+# Unweighted KNN (with no feature selection)
+np.random.seed(123)
+cv_method = RepeatedStratifiedKFold(n_splits=5, n_repeats=5, random_state=123)
+unweighted_no_fs_score = cross_val_score(wrapper,
+                                         x,
+                                         y,
+                                         cv=cv_method,
+                                         scoring='accuracy')
+print(f"unweighted_no_fs_score: {unweighted_no_fs_score.mean():.2f}")
+
+# Unweighted KNN with only the selected features
+np.random.seed(123)
+x_fs = x[:, SpFtWgt_results.get('features')]
+unweighted_fs_score = cross_val_score(wrapper,
+                                      x_fs,
+                                      y,
+                                      cv=cv_method,
+                                      scoring='accuracy')
+print(f"unweighted_fs_score: {unweighted_fs_score.mean():.2f}")
+
+# Weighted KNN with feature selection
+# an independent run for validation
+np.random.seed(123)
+ft_weights = SpFtWgt_results.get('importance')
+x_fs_weighted = ft_weights * x_fs
+weighted_fs_score = cross_val_score(wrapper,
+                                    x_fs_weighted,
+                                    y,
+                                    cv=cv_method,
+                                    scoring='accuracy')
+print(f"weighted_fs_score: {weighted_fs_score.mean():.2f}")
 
 #################################################
 # ##### REGRESSION EXAMPLE  #####################
 #################################################
 
-df = load_boston()
+dataset = 'boston_housing.csv'
+n_neighbors = 1
+n_jobs = -1
+####
+# you should scale y to be between 0 and 1 for the algorithm to work properly!
+x, y = prepare_dataset_for_modeling(dataset, is_classification=False)
+print("dataset = " + dataset + ", shape of x = ", x.shape)
 
-x, y = df.data, df.target
+wrapper = KNeighborsRegressor(n_neighbors=n_neighbors)
 
-# set the engine parameters
-sp_engine = SpFSR(x, y, pred_type='r', scoring='r2', wrapper=DecisionTreeRegressor(random_state=1))
+scoring = 'r2'
 
-sp_run = sp_engine.run(num_features=5)
+sp_engine = SpFtWgt(x=x, y=y, wrapper=wrapper, scoring=scoring)
 
-sp_results = sp_run.results
+# make sure the results are repeatable
+np.random.seed(123)
 
-print('Best value:', sp_results.get('selected_ft_score_mean'))
-print('Indices of selected features: ', sp_results.get('selected_features'))
-print('Importance of selected features: ', sp_results.get('selected_ft_importance').round(3))
-print('Total iterations for the optimal feature set:', sp_results.get('total_iter_for_opt'))
+# for regression problems, you must set stratified_cv to False
+# because the default value of True will not work
+SpFtWgt_results = sp_engine.run(stratified_cv=False, n_jobs=n_jobs).results
 
+print('Best value:', SpFtWgt_results.get('best_value'))
+print('Indices of selected features: ', SpFtWgt_results.get('features'))
+print('Weights of selected features: ', SpFtWgt_results.get('importance').round(3))
+
+#################################################
+# ######   Comparisons   ########################
+#################################################
+
+# Unweighted KNN (with no feature selection)
+np.random.seed(123)
+cv_method = RepeatedKFold(n_splits=5, n_repeats=5, random_state=123)
+unweighted_no_fs_score = cross_val_score(wrapper,
+                                         x,
+                                         y,
+                                         cv=cv_method,
+                                         scoring=scoring)
+print(f"unweighted_no_fs_score: {unweighted_no_fs_score.mean():.3f}")
+
+# Unweighted KNN with only the selected features
+np.random.seed(123)
+x_fs = x[:, SpFtWgt_results.get('features')]
+unweighted_fs_score = cross_val_score(wrapper,
+                                      x_fs,
+                                      y,
+                                      cv=cv_method,
+                                      scoring=scoring)
+print(f"unweighted_fs_score: {unweighted_fs_score.mean():.3f}")
+
+# Weighted KNN with feature selection
+# an independent run for validation
+np.random.seed(123)
+ft_weights = SpFtWgt_results.get('importance')
+x_fs_weighted = ft_weights * x_fs
+weighted_fs_score = cross_val_score(wrapper,
+                                    x_fs_weighted,
+                                    y,
+                                    cv=cv_method,
+                                    scoring=scoring)
+print(f"weighted_fs_score: {weighted_fs_score.mean():.3f}")
