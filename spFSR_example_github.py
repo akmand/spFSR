4c706d76ff0d786277e8be74c7690e86d1301fee
Dr. Vural Aksakalli <vaksakalli@gmail.com>
2021-03-03 00:23:07 +1100

    Update spFSR_example_github.py

--- spFSR_example_github.py
+++ spFSR_example_github.py
@@ -2,7 +2,7 @@
 # SpFSR: Feature Selection and Ranking via SPSA-BB
 # Required Python version >= 3.6
 # V. Aksakalli & Z. D. Yenice
-# GPL-3.0, 2020
+# GPL-3.0, 2021
 # Please refer to below for more information:
 # https://arxiv.org/abs/1804.05589
 
@@ -46,7 +46,7 @@ sp_engine = SpFSR(x, y, pred_type=pred_type, scoring=scoring, wrapper=wrapper)
 # available engine parameters (with default values in parentheses):
 # 1.  num_features (0): number of features to select - a value of 0 results in automatic feature selection
 # 2.  iter_max (100): max number of iterations
-# 3.  stall_limit (30): when to restart the search (up to iter_max) - should be about iter_max/3
+# 3.  stall_limit (35): when to restart the search (up to iter_max) - should be about iter_max/3
 # 4.  n_samples_max (5000): max number of randomly selected observations to be used during search
 #     can be 'None' for using all available observations
 # 5.  ft_weighting (False): if features should be weighted by their importance values - this usually helps with kNN's.
@@ -56,18 +56,19 @@ sp_engine = SpFSR(x, y, pred_type=pred_type, scoring=scoring, wrapper=wrapper)
 #     for example: for a range of 0.2, most important RFI feature will have an imp. value of 0.1 and
 #     the least important will have an imp. value of -0.1 - a value of 0 is also possible and
 #     it will result in all RFI-selected features to have 0 imp. values
-# 8.  gain_type ('bb'): either 'bb' (Barzilai & Borwein) gains or 'mon' (monotone) gains as the step size during search
-# 9.  cv_folds (5): number of folds to use during (perhaps repeated) CV both for evaluation and gradient evaluation
-# 10.  num_grad_avg (4): number of gradient estimates to be averaged for determining search direction
+# 8.  rf_n_estimators (50): number of estimators for hot start RFI as well as for prediction RFI in FILTER mode
+# 9.  gain_type ('bb'): either 'bb' (Barzilai & Borwein) gains or 'mon' (monotone) gains as the step size during search
+# 10.  cv_folds (5): number of folds to use during (perhaps repeated) CV both for evaluation and gradient evaluation
+# 11.  num_grad_avg (4): number of gradient estimates to be averaged for determining search direction
 #     for better gradient estimation, try increasing this number - though this will slow down the search
-# 11. cv_reps_eval (3): number of CV repetitions for evaluating a candidate feature set
-# 12. cv_reps_grad (1): number of CV repetitions for evaluating y-plus and y-minus solutions during gradient estimation
-# 13. stall_tolerance (1e-8): tolerance in objective function change for stalling
-# 14. display_rounding (3): number of digits to display during algorithm execution
-# 15. is_debug (False): whether detailed search info should be displayed for each iteration
-# 16. random_state(1): seed for controlling randomness in the execution of the algorithm
-# 17. n_jobs (1): number of cores to be used in CV - this will be passed into cross_val_score()
-# 18. print_freq (10): iteration print frequency for the algorithm output
+# 12. cv_reps_eval (3): number of CV repetitions for evaluating a candidate feature set
+# 13. cv_reps_grad (1): number of CV repetitions for evaluating y-plus and y-minus solutions during gradient estimation
+# 14. stall_tolerance (1e-8): tolerance in objective function change for stalling
+# 15. display_rounding (3): number of digits to display during algorithm execution
+# 16. is_debug (False): whether detailed search info should be displayed for each iteration
+# 17. random_state(1): seed for controlling randomness in the execution of the algorithm
+# 18. n_jobs (1): number of cores to be used in CV - this will be passed into cross_val_score()
+# 19. print_freq (10): iteration print frequency for the algorithm output
 sp_run = sp_engine.run(num_features=5)
 
 # get the results of the run
